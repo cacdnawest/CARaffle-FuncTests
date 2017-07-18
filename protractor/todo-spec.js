8d534ec020825d6304a57ffa715958aa87ff77ac
cacdnawest <robert.karlin@ca.com>
2017-07-17 22:05:34 -0500

    Update todo-spec.js

--- protractor/todo-spec.js
+++ protractor/todo-spec.js
@@ -1,4 +1,4 @@
-describe('CDBU Modern Software Factory UI Test - xxx Environment', function() {
+describe('The Modern Software Factory Demo UI Test - NA West', function() {
   it('should add a todo', function() {
     browser.ignoreSynchronization = true;
     browser.get('http://nawest-preprod.cdbu.io');
